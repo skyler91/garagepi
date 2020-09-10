eaf54cda1c98256dd6266797c731545207fae73d
Skyler Sell <4495439+skyler91@users.noreply.github.com>
2020-09-10 05:51:24 +0100

    Cleanup and test GPIO code

--- pi/garagedoorgpio.py
+++ pi/garagedoorgpio.py
@@ -3,35 +3,26 @@ import gpiozero
 #import RPi.GPIO as gpio
 import pigpio
 from time import sleep
+from signal import pause
 
 piname = 'garagepi'
 RELAY_PIN = 2
-CLOSE_SENSOR_PIN = 5
+CLOSE_SENSOR_PIN = 4
 
 relay = gpiozero.OutputDevice(RELAY_PIN)
 door_sensor = gpiozero.LineSensor(CLOSE_SENSOR_PIN)
-door_sensor.when_line(door_opened)
-door_sensor.when_no_line(door_closed)
 #gpio.setmode(gpio.BCM)
 #gpio.setwarnings(False)
 #gpio.setup(CLOSE_SENSOR_PIN, gpio.IN)
 
-def door_closed():
-    # Send update to gcloud
-    print("Door closed")
-
-def door_opened():
-    # Send update to gcloud
-    print("Door opened")
-
 def is_door_open():
-    if door_sensor.value < 0.5 :
-        # Door is closed!
-        print("Door is closed")
-        return True
-    else :
+    if door_sensor.value == 0 :
         # Door is open!
         print("Door is open")
+        return True
+    else :
+        # Door is closed!
+        print("Door is closed")
         return False
 
 def open_door():
@@ -46,3 +37,18 @@ def toggle_door():
     relay.on()
     sleep(1)
     relay.off()
+
+def print_open():
+    print("Opened!")
+
+def print_close():
+    print("Closed!")
+
+
+if __name__ == "__main__":
+    door_sensor.when_line = print_open
+    door_sensor.when_no_line = print_close
+    while(True):
+        print(str(door_sensor.value))
+        sleep(0.1)
+    pause()
