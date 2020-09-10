d3d1737270e0613de39abf09af039b1c01f08ee3
Skyler Sell <4495439+skyler91@users.noreply.github.com>
2020-09-09 21:04:54 -0700

    Switch to gpiozero, remove mock

--- pi/garagedoorgpio.py
+++ pi/garagedoorgpio.py
@@ -1,7 +1,7 @@
 import os
 import gpiozero
-import RPi.GPIO as gpio
-import mockgpio
+#import RPi.GPIO as gpio
+import pigpio
 from time import sleep
 
 piname = 'garagepi'
@@ -9,14 +9,23 @@ RELAY_PIN = 2
 CLOSE_SENSOR_PIN = 5
 
 relay = gpiozero.OutputDevice(RELAY_PIN)
-gpio.setmode(gpio.BCM)
-gpio.setwarnings(False)
-gpio.setup(CLOSE_SENSOR_PIN, gpio.IN)
+door_sensor = gpiozero.LineSensor(CLOSE_SENSOR_PIN)
+door_sensor.when_line(door_opened)
+door_sensor.when_no_line(door_closed)
+#gpio.setmode(gpio.BCM)
+#gpio.setwarnings(False)
+#gpio.setup(CLOSE_SENSOR_PIN, gpio.IN)
+
+def door_closed():
+    # Send update to gcloud
+    print("Door closed")
+
+def door_opened():
+    # Send update to gcloud
+    print("Door opened")
 
 def is_door_open():
-    if not isPi :
-        return mockgpio.is_door_open()
-    if gpio.input(CLOSE_SENSOR_PIN) == False :
+    if door_sensor.value < 0.5 :
         # Door is closed!
         print("Door is closed")
         return True
@@ -26,22 +35,14 @@ def is_door_open():
         return False
 
 def open_door():
-    if not isPi:
-        return mockgpio.open_door()
     if not is_door_open():
         toggle_door()
 
 def close_door():
-    if not isPi :
-        return mockgpio.close_door()
     if is_door_open():
         toggle_door()
 
 def toggle_door():
-    if not isPi :
-        return mockgpio.toggle_door()
     relay.on()
     sleep(1)
     relay.off()
-
-isPi = os.uname()[1] == piname
