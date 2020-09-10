7e3005cf0aebfc31d2b083c7d9613e4e841bf670
Skyler Sell <4495439+skyler91@users.noreply.github.com>
2020-09-09 19:30:06 -0700

    Add folder structure, open/close functions

--- pi/garagedoorgpio.py
+++ pi/garagedoorgpio.py
@@ -10,25 +10,25 @@ gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
 gpio.setup(CLOSE_SENSOR_PIN, gpio.IN)
 
-doorClosed = True
-
 def is_door_open():
-    #return gpio.input(CLOSE_SENSOR_PIN)
-    
     if gpio.input(CLOSE_SENSOR_PIN) == False :
         # Door is closed!
-        if not doorClosed :
-            print("Door closed")
-            doorClosed = True
+        print("Door is closed")
+        return True
     else :
         # Door is open!
-        if doorClosed :
-            print("Door open")
-            doorClosed = False
-    return not doorClosed
+        print("Door is open")
+        return False
+
+def open_door():
+    if not is_door_open():
+        toggle_door()
+
+def close_door():
+    if is_door_open():
+        toggle_door()
 
 def toggle_door():
     relay.on()
     sleep(1)
     relay.off()
-    doorClosed = not doorClosed
