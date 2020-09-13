ef30c4085e79095d339820f8a5d649a2878bb01d
Skyler Sell <4495439+skyler91@users.noreply.github.com>
2020-09-13 16:12:45 -0700

    Update scripts to work with crontab, add requirements.txt

--- pi/startsubscriber.sh
+++ pi/startsubscriber.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cd "$(dirname ${0%\*})"
 export GOOGLE_APPLICATION_CREDENTIALS=garagepi-pub.json
-python3 -c 'import gcloud; gcloud.subscribe_to_garagecommand()'
+/usr/bin/python3 -c 'import gcloud; gcloud.subscribe_to_garagecommand()'
